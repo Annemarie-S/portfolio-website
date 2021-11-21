960e14275c941e5f82a95c3a9edaeccb41133d63
Annemarie-S <93160352+Annemarie-S@users.noreply.github.com>
2021-11-21 21:10:44 +0100

    add nav function hamburger

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,24 @@
-console.log("Welcome to my portfolio, nice to see you here!")
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+};
+
+console.log("Welcome to my portfolio, nice to see you here!");
+
+var person = {
+  name: "Annemarie",
+  age: 39,
+  hobbies: "photography"
+};
+
+console.log(person.name);
+console.log(person.age);
+console.log(person.hobbies);
+
+for (var i = 1; i <= 100; i++) {
+  console.log(i);
+};
